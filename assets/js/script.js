cdca50741d00be0d5e890eed3bb53a891bd1034b
Joshua Almodovar <joshuarosamktp4@gmail.com>
2021-05-19 22:51:14 -0400

    add dragability to tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -153,3 +153,41 @@ $("#remove-tasks").on("click", function () {
 
 // load tasks for the first time
 loadTasks();
+
+// makes tasks movable
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function (event) {
+    console.log("activate", this);
+  },
+  deactivate: function (event) {
+    console.log("deactivate", this);
+  },
+  over: function () {
+    console.log("over", EventTarget);
+  },
+  update: function (event) {
+    var tempArr = [];
+
+    $(this)
+      .children()
+      .each(function () {
+        var text = $(this).find("p").text().trim();
+
+        var date = $(this).find("span").text().trim();
+
+        tempArr.push({
+          text: text,
+          date: date,
+        });
+      });
+    console.log(tempArr);
+    var arrName = $(this).attr("id").replace("list-", "");
+
+    tasks[arrName] = tempArr;
+    saveTasks();
+  },
+});
